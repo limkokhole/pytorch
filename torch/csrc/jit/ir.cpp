16f4ef66fee47298e5cd87fc31db8673b8bbe660
Michael Suo <suo@fb.com>
2019-04-01 17:38:56 -0700

    [jit] fix side-effects and aliasing for custom ops
    Previously we didn't track aliasing, mutation, or side effects for
    custom ops. This PR adds in guards with the most conservative
    assumptions possible: the op will 1) have side effects, 2) write to
    everything, and 3) produce a wildcard. We can reign in those
    restrictions later, but want to get something correct for now.
    
    It's a little weird because we have no way of knowing whether a given
    node represents a custom op--I added it to the schema but I'm open to
    suggestions for a better place.
    
    Follow-ups to this:
    - Users should be able to specify aliasing annotations/side effects to
    get better perf somehow.
    - Relax assumptions a bit.
    
    gh-metadata: pytorch pytorch 18711 gh/suo/15/head

--- torch/csrc/jit/ir.cpp
+++ torch/csrc/jit/ir.cpp
@@ -846,9 +846,15 @@ bool Node::hasSideEffects() const {
     case prim::SetAttr:
     case aten::warn:
     case prim::AddStatValue:
-     case prim::TimePoint:
+    case prim::TimePoint:
       return true;
   }
+  // Custom ops may have arbitrary side effects
+  const auto schema = maybeSchema();
+  if (schema && schema->is_inferred_from_custom_op()) {
+    return true;
+  }
+
   return false;
 }
 
